5faa9b2f3f64fc0136d558e6c6fe4245ee464962
SomersetBlake <BlakeSomerset373@proton.me>
2024-03-10 21:38:41 +0100

    Added query file for CPP

--- lua/dye-brackets/init.lua
+++ lua/dye-brackets/init.lua
@@ -2,8 +2,6 @@ local ts_utils = require("nvim-treesitter.ts_utils")
 local M = {}
 
 local highlights = require("dye-brackets.highlight-groups")
-local query_line = '(compound_statement  "{" @openBracket "}" @closedBracket) @brackets'
-
 
 local function create_namespace()
 	return vim.api.nvim_create_namespace('')
@@ -18,18 +16,16 @@ end
 
 
 local function highlightNodes(bufNb, namespace, nodes)
-	print(nodes)
 	local hl_groups, len = highlights.getHlGroups()
 	local hl_nb = 1
 	for _, node in pairs(nodes) do
 		if(hl_nb == len+1) then
 			hl_nb = 1
 		end
-		if node[1]:type() == '{' then
-			highlightNode(bufNb, namespace, hl_groups[hl_nb], node[1])
-		end
-		if node[2]:type() == '}' then
-			highlightNode(bufNb, namespace, hl_groups[hl_nb], node[2])
+		for element, block in pairs(node) do
+			if(element<3) then
+				highlightNode(bufNb, namespace, hl_groups[hl_nb], block)
+			end
 		end
 		hl_nb = hl_nb + 1
 	end
@@ -40,8 +36,8 @@ M.select = function()
 	local bufferNb = vim.api.nvim_get_current_buf()
 	local namespace = create_namespace()
 	local parser = require('nvim-treesitter.parsers').get_parser()
-	local query = vim.treesitter.query.parse(parser:lang(), query_line)
 	local root = parser:parse()[1]:root()
+	local query = vim.treesitter.query.get("cpp", "cpp-query")
 	local nodes = {}
 	for _, n, _ in query:iter_matches(root, bufferNb) do
 		table.insert(nodes, n)
---
+++ queries/cpp/cpp-query.scm
@@ -0,0 +1,9 @@
+(compound_statement "{" @openBracket "}" @closedBracket) @brackets
+
+(argument_list "(" @openBracket ")" @closedBracket) @brackets
+
+(parameter_list "(" @openBracket ")" @closedBracket) @brackets
+
+(parenthesized_expression "(" @openBracket ")" @closedBracket) @brackets
+
+(for_statement "(" @openBracket ")" @closedBracket) @brackets
